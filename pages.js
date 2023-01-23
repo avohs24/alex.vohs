8148eec5d9f7f3fcc4344463257e78e7b0b48c09
Alex Vohs <alex.vohs@warnermedia>
2023-01-23 16:19:13 -0500

    deployment

--- pages.js
+++ pages.js
@@ -1,11 +1,11 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/avohs24/alex.vohs.github.io.git/home";
 
 ghpages.publish(
   pathname,
   {
-    branch: "master",
+    branch: "main",
     repo: repoURL,
   },
   (err) => {
